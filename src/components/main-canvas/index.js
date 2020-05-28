8983bb764e0fade0a6762c05268474de64c688e6
Usama Sulaiman <sulaiman.usama@gmail.com>
2020-05-28 13:48:20 +0530

    test commit

--- src/components/main-canvas/index.js
+++ src/components/main-canvas/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Canvas } from 'react-three-fiber'
 
 export default function MainCanvas() {
+  const canvasRef = useRef(null)
+  // console.log('canvas stream', canvasRef.currents);
   return (
     <div className="canvas-container">
       <div className="my-canvas">
         {/* <canvas id="myCanvas" /> */}
-        <Canvas>
+        <Canvas ref={canvasRef}>
           <pointLight position={[10, 10, 10]} />
           <mesh>
             <sphereBufferGeometry attach="geometry" />
