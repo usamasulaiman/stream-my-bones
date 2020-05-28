20ebfb1794a46ec6ebb5eb1041629da7b44345e4
Usama Sulaiman <sulaiman.usama@gmail.com>
2020-05-29 01:27:47 +0530

    Changes - Adds drei package - Loads external model

--- src/components/main-canvas/index.js
+++ src/components/main-canvas/index.js
@@ -1,13 +1,12 @@
-import React, { useRef, Suspense, useEffect } from 'react';
-import { Canvas, useFrame, useThree, extend } from 'react-three-fiber';
-import { useLoader } from 'react-three-fiber';
+import React, { useRef, Suspense } from 'react';
+import { Canvas, useLoader } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-extend({ OrbitControls });
+import { OrbitControls } from "drei";
 
-function Asset({ url }) {
-  const gltf = useLoader(GLTFLoader, url)
-  return <primitive object={gltf.scene} dispose={null} />
+function Skeleton({ url }) {
+  const gltf = useLoader(GLTFLoader, url);
+  console.log("url", url, GLTFLoader, gltf);
+  return <primitive object={gltf.scene} position={[0, -7, 0]} dispose={null} />;
 }
 
 function RandomShape() {
@@ -23,13 +22,6 @@ function RandomShape() {
   );
 }
 
-const Controls = ({props}) => {
-  const { camera } = useThree()
-  const controls = useRef()
-  useFrame(() => controls.current && controls.current.update())
-  return <orbitControls ref={controls} args={[camera]} enableDamping dampingFactor={0.1} />
-}
-
 
 export default function MainCanvas() {
   return (
@@ -37,9 +29,9 @@ export default function MainCanvas() {
       <div className="my-canvas">
         <Canvas >
           <directionalLight position={[10, 10, 10]} />
-          <Controls />
+          <OrbitControls />
           <Suspense fallback={<RandomShape />}>
-            <Asset url="/assets/scene.gltf" />
+            <Skeleton url="/assets/skeleton.glb" />
           </Suspense>
         </Canvas>
       </div>
