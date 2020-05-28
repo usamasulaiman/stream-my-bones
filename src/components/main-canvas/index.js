f9063fef1e22409a7ed52c6978db5dd5187ba373
Usama Sulaiman <sulaiman.usama@gmail.com>
2020-05-28 23:07:44 +0530

    Gets control of mesh

--- src/components/main-canvas/index.js
+++ src/components/main-canvas/index.js
@@ -1,21 +1,48 @@
-import React, { useRef } from 'react';
-import { Canvas } from 'react-three-fiber'
+import React, { useRef, Suspense, useEffect } from 'react';
+import { Canvas, useFrame, useThree, extend } from 'react-three-fiber';
+import { useLoader } from 'react-three-fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+extend({ OrbitControls });
+
+function Asset({ url }) {
+  const gltf = useLoader(GLTFLoader, url)
+  return <primitive object={gltf.scene} dispose={null} />
+}
+
+function RandomShape() {
+  const canvasRef = useRef();
+  // Rotate mesh every frame, this is outside of React without overhead
+  // useFrame(() => (canvasRef.current.rotation.x = canvasRef.current.rotation.y += 0.01));
+
+  return (
+    <mesh ref={canvasRef}>
+      <coneBufferGeometry attach="geometry" />
+      <meshStandardMaterial attach="material" color="0xff0000" />
+    </mesh>
+  );
+}
+
+const Controls = ({props}) => {
+  const { camera } = useThree()
+  const controls = useRef()
+  useFrame(() => controls.current && controls.current.update())
+  return <orbitControls ref={controls} args={[camera]} enableDamping dampingFactor={0.1} />
+}
+
 
 export default function MainCanvas() {
-  const canvasRef = useRef(null)
-  // console.log('canvas stream', canvasRef.currents);
   return (
     <div className="canvas-container">
       <div className="my-canvas">
-        {/* <canvas id="myCanvas" /> */}
-        <Canvas ref={canvasRef}>
-          <pointLight position={[10, 10, 10]} />
-          <mesh>
-            <sphereBufferGeometry attach="geometry" />
-            <meshStandardMaterial attach="material" color="hotpink" />
-          </mesh>
+        <Canvas >
+          <directionalLight position={[10, 10, 10]} />
+          <Controls />
+          <Suspense fallback={<RandomShape />}>
+            <Asset url="/assets/scene.gltf" />
+          </Suspense>
         </Canvas>
       </div>
     </div>
-  )
+  );
 }
